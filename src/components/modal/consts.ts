99b95ca6e9c13db42f32b3984cdc0301a59d8c3b
luis.couto <luis.couto@frgnet.com.br>
2024-03-20 16:47:47 -0300

    fix(modal): ajustado import e export

--- src/components/modal/consts.ts
+++ src/components/modal/consts.ts
@@ -1,13 +1,2 @@
-import { ModalSizes } from "./types";
-
 export const MODAL_CLOSE_ANIMATION_MODAL_TIME = 100;
 export const MODAL_HEADER_ID = "base__modal__header";
-
-export const MODAL_SIZES: { [key in ModalSizes]: string | undefined } = {
-  xs: "15rem",
-  sm: "20rem",
-  md: "30rem",
-  lg: "50rem",
-  full: "95vw",
-  auto: undefined,
-};
